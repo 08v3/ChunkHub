c9651f5e17cb37bec7b9208ad32b56eee55a1164
08v3 <nbd69420@gmail.com>
2025-08-30 17:42:56 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -6,7 +6,7 @@
  \____|_| |_|\___/|_| \_|_|\_\ |_| |_|\___/|____/ 
 ]]
 if game.CreatorId == 35102746 then -- Fish It
-loadstring(game:HttpGet("https://cdn.authguard.org/virtual-file/27fd2435452a4eb59f7960dd408048a2"))()
+loadstring(game:HttpGet("https://cdn.authguard.org/virtual-file/8a0754111c4748238bcfc4ffcd271421"))()
 elseif game.CreatorId == 32333865 then -- Bl
 loadstring(game:HttpGet("https://cdn.authguard.org/virtual-file/3a90150860154be9a35ee96a4d1adc84"))()
 end
