f9aaa7a2e022b0beb8500d380a4865a1dea862f6
08v3 <nbd69420@gmail.com>
2025-08-30 23:10:38 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -9,4 +9,7 @@ if game.CreatorId == 35102746 then -- Fish It
 loadstring(game:HttpGet("https://cdn.authguard.org/virtual-file/8a0754111c4748238bcfc4ffcd271421"))()
 elseif game.CreatorId == 32333865 then -- Bl
 loadstring(game:HttpGet("https://cdn.authguard.org/virtual-file/3a90150860154be9a35ee96a4d1adc84"))()
+elseif game.CreatorId == 4372130 then -- Blox Fruits
+loadstring(game:HttpGet("https://cdn.authguard.org/virtual-file/275ae051c38e4e459130796d710bb09c"))()
 end
+print("Chunk hub loader")
