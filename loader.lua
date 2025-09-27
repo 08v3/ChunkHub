e3e639c48e9b9debd66f3efc2ada5eb118fc274e
08v3 <nbd69420@gmail.com>
2025-09-27 11:14:39 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -5,9 +5,6 @@
 | |___|  _  | |_| | |\  | . \  |  _  | |_| | |_) |
  \____|_| |_|\___/|_| \_|_|\_\ |_| |_|\___/|____/ 
 ]]
-if isfile("ChunkHubKey.txt") then
-    script_key = readfile("ChunkHubKey.txt")
-end
 local uis = game:GetService("UserInputService")
 local p = game:GetService("Players")
 local Lighting = game:GetService("Lighting")
@@ -56,138 +53,171 @@ game:GetService("StarterGui"):SetCore("SendNotification", {
     Title = "[ℹ️] Greetings",
     Text = "Welcome to Chunk Hub, "..lp.Name.."."
 })
-local sgui = Instance.new("ScreenGui")
-sgui.Name = "ScreenGui"
-sgui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-sgui.ResetOnSpawn = false
-sgui.Parent = lp.PlayerGui
-local MainFrame = Instance.new("Frame")
-MainFrame.Name = "KeySystem"
-MainFrame.Size = UDim2.new(0.375, 0, 0.42135, 0)
-MainFrame.Position = UDim2.new(0.3075, 0, 0.23034, 0)
-MainFrame.BackgroundColor3 = Color3.fromRGB(46, 46, 46)
-MainFrame.BorderSizePixel = 0
-MainFrame.Active = true
-MainFrame.Draggable = true
-MainFrame.Parent = sgui
-local MainFrameCorner = Instance.new("UICorner")
-MainFrameCorner.CornerRadius = UDim.new(0, 20)
-MainFrameCorner.Parent = MainFrame
-local Key = Instance.new("TextBox")
-Key.Name = "Key"
-Key.Size = UDim2.new(0.92667, 0, 0.4, 0)
-Key.Position = UDim2.new(0.03333, 0, 0.06667, 0)
-Key.BackgroundColor3 = Color3.fromRGB(41, 201, 41)
-Key.BorderSizePixel = 0
-Key.Text = ""
-Key.PlaceholderText = "PUT YOUR KEY HERE"
-Key.PlaceholderColor3 = Color3.fromRGB(0, 0, 0)
-Key.TextColor3 = Color3.fromRGB(0, 0, 0)
-Key.TextWrapped = true
-Key.TextXAlignment = Enum.TextXAlignment.Left
-Key.TextDirection = Enum.TextDirection.LeftToRight
-Key.ClearTextOnFocus = false
-Key.Parent = MainFrame
-if isfile("ChunkHubKey.txt") then
-    Key.Text = readfile("ChunkHubKey.txt")
+local sgui = game:GetService("StarterGui")
+function loader()
+ if game.CreatorId == 35102746 then -- Fish It
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/08v3/ChunkHub/refs/heads/main/fishit.txt"))()
+ elseif game.CreatorId == 32333865 then -- BL
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/08v3/ChunkHub/refs/heads/main/bl.txt"))()
+ elseif game.CreatorId == 4372130 then -- Blox Fruits
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/08v3/ChunkHub/refs/heads/main/bloxfruits.txt"))()
+ elseif game.CreatorId == 2613928 then -- Arsenal
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/08v3/ChunkHub/refs/heads/main/arsenal.txt"))()
+ elseif game.CreatorId == 947178762 then -- Case Rolling RNG
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/08v3/ChunkHub/refs/heads/main/caserollingrng.txt"))()
+ elseif game.PlaceId == 3101667897 then -- Legends Of Speed
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/08v3/ChunkHub/refs/heads/main/legendofspeed.txt"))()
+ elseif game.CreatorId == 4705120 then -- Ninja Legends
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/08v3/ChunkHub/refs/heads/main/ninjalegends.txt"))()
+ elseif game.CreatorId == 35871968 then -- Untitled Fishing Game
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/08v3/ChunkHub/refs/heads/main/untitledfishinggame.txt"))()
+ elseif game.CreatorId == 36097435 then -- Tennis Clash
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/08v3/ChunkHub/refs/heads/main/tennisclash.txt"))()
+ elseif game.CreatorId == 6042520 then -- 99 Night
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/08v3/ChunkHub/refs/heads/main/99n.txt"))()
+ elseif game.CreatorId == 35815907 then -- Sab
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/08v3/ChunkHub/refs/heads/main/sab.txt"))()
+ elseif game.CreatorId == 35789249 then -- GaG
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/08v3/ChunkHub/refs/heads/main/gag.txt"))()
+ elseif game.CreatorId == 12836673 then -- Blade Ball
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/08v3/ChunkHub/refs/heads/main/bladeball.txt"))()
+ elseif game.CreatorId == 295182 then -- Adopt Me
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/08v3/ChunkHub/refs/heads/main/adoptme.txt",true))()
+ else
+  game.Players.LocalPlayer:Kick("Unsupported game")
+ end
 end
-local KeyCorner = Instance.new("UICorner")
-KeyCorner.CornerRadius = UDim.new(0, 20)
-KeyCorner.Parent = Key
-local Submit = Instance.new("TextButton")
-Submit.Name = "Submit"
-Submit.Size = UDim2.new(0.42667, 0, 0.30667, 0)
-Submit.Position = UDim2.new(0.03333, 0, 0.6, 0)
-Submit.BackgroundColor3 = Color3.fromRGB(46, 201, 46)
-Submit.BorderSizePixel = 0
-Submit.Text = "Submit Key"
-Submit.TextColor3 = Color3.fromRGB(0, 0, 0)
-Submit.Parent = MainFrame
-local SubmitCorner = Instance.new("UICorner")
-SubmitCorner.CornerRadius = UDim.new(0, 20)
-SubmitCorner.Parent = Submit
-local Get = Instance.new("TextButton")
-Get.Name = "Get"
-Get.Size = UDim2.new(0.42667, 0, 0.30667, 0)
-Get.Position = UDim2.new(0.53333, 0, 0.6, 0)
-Get.BackgroundColor3 = Color3.fromRGB(46, 201, 46)
-Get.BorderSizePixel = 0
-Get.Text = "Get Key"
-Get.TextColor3 = Color3.fromRGB(0, 0, 0)
-Get.Parent = MainFrame
-local GetCorner = Instance.new("UICorner")
-GetCorner.CornerRadius = UDim.new(0, 20)
-GetCorner.Parent = Get
-local Title = Instance.new("TextLabel")
-Title.Name = "Title"
-Title.Size = UDim2.new(1, 0, 0.30667, 0)
-Title.Position = UDim2.new(0, 0, 0.36667, 0)
-Title.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-Title.BackgroundTransparency = 1
-Title.BorderSizePixel = 0
-Title.Text = "Chunk Hub Key System"
-Title.TextColor3 = Color3.fromRGB(156, 119, 84)
-Title.Parent = MainFrame
-task.spawn(function()
-    function loader()
-        if game.CreatorId == 35102746 then -- Fish It
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/08v3/ChunkHub/refs/heads/main/fishit.txt"))()
-        elseif game.CreatorId == 32333865 then -- BL
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/08v3/ChunkHub/refs/heads/main/bl.txt"))()
-        elseif game.CreatorId == 4372130 then -- Blox Fruits
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/08v3/ChunkHub/refs/heads/main/bloxfruits.txt"))()
-        elseif game.CreatorId == 2613928 then -- Arsenal
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/08v3/ChunkHub/refs/heads/main/arsenal.txt"))()
-        elseif game.CreatorId == 947178762 then -- Case Rolling RNG
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/08v3/ChunkHub/refs/heads/main/caserollingrng.txt"))()
-        elseif game.PlaceId == 3101667897 then -- Legends Of Speed
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/08v3/ChunkHub/refs/heads/main/legendofspeed.txt"))()
-        elseif game.CreatorId == 4705120 then -- Ninja Legends
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/08v3/ChunkHub/refs/heads/main/ninjalegends.txt"))()
-        elseif game.CreatorId == 35871968 then -- Untitled Fishing Game
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/08v3/ChunkHub/refs/heads/main/untitledfishinggame.txt"))()
-        elseif game.CreatorId == 36097435 then -- Tennis Clash
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/08v3/ChunkHub/refs/heads/main/tennisclash.txt"))()
-        elseif game.CreatorId == 6042520 then -- 99 Night
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/08v3/ChunkHub/refs/heads/main/99n.txt"))()
-        elseif game.CreatorId == 35815907 then -- Sab
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/08v3/ChunkHub/refs/heads/main/sab.txt"))()
-        elseif game.CreatorId == 35789249 then -- GaG
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/08v3/ChunkHub/refs/heads/main/gag.txt"))()
-        elseif game.CreatorId == 12836673 then -- Blade Ball
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/08v3/ChunkHub/refs/heads/main/bladeball.txt"))()
-        elseif game.CreatorId == 295182 then -- Adopt Me
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/08v3/ChunkHub/refs/heads/main/adoptme.txt",true))()
-        else
-            game.Players.LocalPlayer:Kick("Unsupported game")
-        end
-    end
-    if script_key and type(script_key) == 'string' and script_key:find("08v3_") then
-        game:GetService("StarterGui"):SetCore("SendNotification", {
-            Title = "[ℹ️] Uiless:",
-            Text = "Loading script with key: "..tostring(script_key)
-        })
-        sgui:Destroy()
-        task.wait(2)
-        loader()
-    end
-    Submit.MouseButton1Click:Connect(function()
-        game:GetService("StarterGui"):SetCore("SendNotification", {
-            Title = "[ℹ️] Submit Key:",
-            Text = "Validating key: "..tostring(Key.Text)
-        })
-        script_key = Key.Text
-        task.wait(2)
-        loader()
-        sgui:Destroy()
-        task.wait(10)
-        writefile("ChunkHubKey.txt", Key.Text)
-    end)
-    Get.MouseButton1Click:Connect(function()
-        setclipboard("https://authguard.org/a/541")
-        game:GetService("StarterGui"):SetCore("SendNotification", {
-            Title = "[ℹ️] Get Key:",
-            Text = "Copied get key url to clipboard"
-        })
-    end)
+if isfile("chunkhub_key.txt") then
+ script_key = readfile("chunkhub_key.txt")
+end
+if type(script_key) == 'string' and string.match(script_key, "08v3_") then
+ sgui:SetCore("SendNotification", {
+  Title = "[ℹ️] Found Script Key:",
+  Text = "Loading main script"
+ })
+ loader()
+ return
+end
+local a = Instance.new("ScreenGui")
+a.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+a.ResetOnSpawn = false
+a.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+local b = Instance.new("Frame")
+b.BorderSizePixel = 0
+b.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+b.Size = UDim2.new(0, 300, 0, 160)
+b.Position = UDim2.new(0, 248, 0, 100)
+b.Active = true
+b.Draggable = true
+b.Parent = a
+local c = Instance.new("ImageLabel")
+c.BorderSizePixel = 0
+c.BackgroundTransparency = 1
+c.Image = "rbxassetid://115846030699084"
+c.Size = UDim2.new(1, 0, 1, 0)
+c.Position = UDim2.new(0, 0, 0, 0)
+c.Parent = b
+local d = Instance.new("TextLabel")
+d.Size = UDim2.new(1, -40, 0, 30)
+d.Position = UDim2.new(0, 10, 0, 5)
+d.BackgroundTransparency = 1
+d.TextScaled = true
+d.Font = Enum.Font.GothamBold
+d.TextColor3 = Color3.fromRGB(0, 0, 0)
+d.TextSize = 20
+d.Parent = b
+local e = os.date("*t").hour
+local f
+if e < 12 then
+ f = "🌤️Good Morning"
+elseif e < 18 then
+ f = "☀️Good Afternoon"
+elseif e < 21 then
+ f = "⛅️Good Evening"
+else
+ f = "🌙Good Night"
+end
+local g = game.Players.LocalPlayer
+d.Text = f .. ", " .. g.Name .. "!"
+local h = Instance.new("TextBox")
+h.Size = UDim2.new(1, -20, 0, 30)
+h.Position = UDim2.new(0, 10, 0, 50)
+h.PlaceholderText = "Enter Key Here"
+h.PlaceholderColor3 = Color3.fromRGB(180, 180, 180)
+h.Text = ""
+h.Font = Enum.Font.Gotham
+h.TextColor3 = Color3.fromRGB(255, 255, 255)
+h.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
+h.TextStrokeTransparency = 1
+h.ClearTextOnFocus = false
+h.TextWrapped = true
+h.Parent = b
+if isfile("chunkhub_key.txt") then
+ h.Text = readfile("chunkhub_key.txt")
+end
+local i = Instance.new("TextButton")
+i.Size = UDim2.new(0.5, -15, 0, 30)
+i.Position = UDim2.new(0, 10, 0, 90)
+i.Text = "Check Key"
+i.Font = Enum.Font.GothamBold
+i.TextColor3 = Color3.fromRGB(255, 255, 255)
+i.BackgroundColor3 = Color3.fromRGB(50, 150, 50)
+i.Parent = b
+local j = Instance.new("TextButton")
+j.Size = UDim2.new(0.5, -15, 0, 30)
+j.Position = UDim2.new(0.5, 5, 0, 90)
+j.Text = "Get Key"
+j.Font = Enum.Font.GothamBold
+j.TextColor3 = Color3.fromRGB(255, 255, 255)
+j.BackgroundColor3 = Color3.fromRGB(50, 100, 200)
+j.Parent = b
+local k = Instance.new("TextButton")
+k.Size = UDim2.new(0, 25, 0, 25)
+k.Position = UDim2.new(1, -30, 0, 5)
+k.Text = "X"
+k.Font = Enum.Font.GothamBold
+k.TextColor3 = Color3.fromRGB(255, 255, 255)
+k.BackgroundColor3 = Color3.fromRGB(200, 50, 50)
+k.Parent = b
+function l(m)
+ local n = Instance.new("UICorner")
+ n.CornerRadius = UDim.new(0, 6)
+ n.Parent = m
+end
+l(h)
+l(i)
+l(j)
+l(k)
+k.MouseButton1Click:Connect(function()
+ a:Destroy()
+end)
+i.MouseButton1Click:Connect(function()
+ script_key = h.Text
+ if not string.match(h.Text, "08v3_") then
+  warn("wrong key format blud")
+  return
+ end
+ local AuthGuard = loadstring(game:HttpGet("https://cdn.authguard.org/virtual-file/c4af0dca5af94ca28cb9ba9fe8a5f9fa"))()
+ local start = os.clock()
+ if AuthGuard.CheckKey() then
+  loader()
+  writefile("chunkhub_key.txt", h.Text)
+  a:Destroy()
+ else
+  sgui:SetCore("SendNotification", {
+   Title = "[ℹ️] Check Key:",
+   Text = "Unauthorized"
+  })
+  if isfile("chunkhub_key.txt") then
+   delfile("chunkhub_key.txt")
+  end
+  return
+ end
+end)
+j.MouseButton1Click:Connect(function()
+ sgui:SetCore("SendNotification", {
+  Title = "[ℹ️] Get Key:",
+  Text = "Copied to clipboard"
+ })
+ setclipboard("https://authguard.org/a/541")
 end)
